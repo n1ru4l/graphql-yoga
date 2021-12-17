ab37dd187891aca836853f5656f2b3ddae6d45df
Saihajpreet Singh <saihajpreet.singh@gmail.com>
2021-12-16 21:43:16 -0600

    start nextjs example

---
+++ examples/nextjs/pages/api/graphql.ts
@@ -0,0 +1,24 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import { GraphQLServer } from 'graphql-yoga'
+import { NextApiRequest, NextApiResponse } from 'next'
+
+const server = new GraphQLServer({
+  typeDefs: 'type Query {hello: String!}',
+  resolvers: { Query: { hello: () => 'Hello World' } },
+  cors: false,
+  endpoint: '/api/graphql',
+})
+
+export const config = {
+  api: {
+    bodyParser: false,
+    externalResolver: true,
+  },
+}
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse,
+) {
+  return server.requestListener(req, res)
+}
